5cc88c2fd45fe43df3c622ee7bb81d5480134a60
Muhammad Rama Nurimani <124318936+muhammadrama19@users.noreply.github.com>
2025-06-28 15:44:12 +0700

    Update some work experience and project update

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -9,6 +9,7 @@ import imageWork3 from "../../assets/Desktop Mockup 07.png";
 import imageWork4 from "../../assets/tttt.png";
 import imageWork5 from "../../assets/DDDD.png";
 import imageWork6 from "../../assets/MovieMess.png";
+import imageWork7 from "../../assets/sipta_img.png";
 import { Links } from "react-router-dom";
 
 const About = () => {
@@ -23,24 +24,30 @@ const About = () => {
           if (index !== -1) {
             setHoveredStates((prevState) => {
               const newState = [...prevState];
-              newState[index] = entry.isIntersecting; 
+              newState[index] = entry.isIntersecting;
               return newState;
             });
           }
         });
       },
-      { threshold: 0.5 } 
+      { threshold: 0.5 }
     );
-  
+
     subtitleRefs.current.forEach((ref) => ref && observer.observe(ref));
-  
+
     return () => {
       subtitleRefs.current.forEach((ref) => ref && observer.unobserve(ref));
     };
   }, []);
-  
 
   const experiences = [
+    {
+      date: "June 2025 - Present",
+      title: "Backend Engineer at PT. Neural Technologies Indonesia",
+      description: [
+        "Currently working as backend engineer using Odoo with Python and PostgreSQL developing for ERP System.",
+      ],
+    },
     {
       date: "Feb 2024 - Oktober 2024",
       title: "Front-End Developer - Politeknik Negeri Bandung",
@@ -48,7 +55,7 @@ const About = () => {
         "Developed the front end for a touchless exhibition app utilizing Kinect sensors as part of a lecturer's project research.",
         "Assisted with backend tasks, including migrating and organizing data from spreadsheets into structured SQL files using Python.",
       ],
-      badges: ["HTML", "JavaScript", "CSS", "Python", "PostgreSQL"]
+      badges: ["HTML", "JavaScript", "CSS", "Python", "PostgreSQL"],
     },
     {
       date: "July 2024 - August 2024",
@@ -63,6 +70,13 @@ const About = () => {
   ];
   const projects = [
     {
+      src: imageWork7,
+      title: "Sistem Pengelolaan Tugas Akhir Mahasiswa",
+      description:
+        "Sistem Pengelolaan Tugas Akhir Mahasiswa is a web application designed to manage student final projects. It allows managing end to end activity of final project in Computer Software Engineering Department, Politeknik Negeri Bandung more centralized and digitalized. As developer I work closely with Devops (Integrator) and Quality Assurance team developing feature, first, I developed the feature for viewing any lecturer who has been filled the FTA01 form, then I developed feature allowing student to view the lecturer schedule for final project consultation, also I assit in developing lecturer allocation for final project examiner. This project involve end to end development from Requirement Engineering, Design and Prototyping interface, Development, Testing from QA team and continous feedback from User Acceptance Testing (UAT) from the user.",
+      badges: ["Laravel", "PHP", "MySQL", "Bootstrap"],
+    },
+    {
       src: imageWork6,
       title: "Movie Mess",
       description:
@@ -73,7 +87,7 @@ const About = () => {
         "Prisma",
         "Supabase",
         "Tailwind CSS",
-        "ShadCN UI"
+        "ShadCN UI",
       ],
       links: [
         {
@@ -82,9 +96,9 @@ const About = () => {
         },
         {
           label: "Repository",
-          url: "https://github.com/muhammadrama19/movie-message"
-        }
-      ]
+          url: "https://github.com/muhammadrama19/movie-message",
+        },
+      ],
     },
     {
       src: myImage,
@@ -113,9 +127,9 @@ const About = () => {
         },
         {
           label: "GitHub",
-          url: "https://github.com/muhammadrama19/webdev2024"
-        }
-      ]
+          url: "https://github.com/muhammadrama19/webdev2024",
+        },
+      ],
     },
     {
       src: imageWork2,
@@ -135,8 +149,8 @@ const About = () => {
         {
           label: "Dashboard",
           url: "https://www.figma.com/proto/KmF4zQNrKP2cQf9Cc2fWMw/Rama's-file?node-id=15-759&p=f&t=V1KPT1k5Lo0KSpmQ-0&scaling=scale-down-width&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=15%3A759",
-        }
-      ]
+        },
+      ],
     },
     {
       src: imageWork3,
@@ -148,8 +162,8 @@ const About = () => {
         {
           label: "Game",
           url: "https://heaplow.itch.io/aku-si-pemburu-kuyang",
-        }
-      ]
+        },
+      ],
     },
     {
       src: imageWork4,
@@ -161,8 +175,8 @@ const About = () => {
         {
           label: "Website",
           url: "https://muhammadrama19.github.io/votehub/",
-        }
-      ]
+        },
+      ],
     },
     {
       src: imageWork5,
@@ -173,9 +187,9 @@ const About = () => {
       links: [
         {
           label: "GitLab",
-          url: "https://gitlab.com/segfault_tubes_sem1/tictactwo_segfault_tubes"
-        }
-      ]
+          url: "https://gitlab.com/segfault_tubes_sem1/tictactwo_segfault_tubes",
+        },
+      ],
     },
   ];
   return (
@@ -183,7 +197,7 @@ const About = () => {
       <Row className="mobile-only">
         <Col xs={12} sm={12} md={12} className="mx-3">
           <h2
-            ref={(el) => (subtitleRefs.current[0] = el)} 
+            ref={(el) => (subtitleRefs.current[0] = el)}
             className={`about-subtitle ${hoveredStates[0] ? "hovered" : ""}`}
           >
             About
@@ -194,7 +208,7 @@ const About = () => {
       <Row className="mb-4" id="about">
         <Col xs={12} sm={12} md={12} className="mx-3">
           <p className="about-description">
-            Hi, I'm Rama – a passionate and driven Front-End Developer with
+            Hi, I'm Rama – a passionate and driven Fullstack Engineer with
             experience in crafting user-focused web applications. My journey
             began with university projects, where I collaborated on various
             group assignments, and extended to freelancing on research-focused
@@ -202,12 +216,12 @@ const About = () => {
           </p>
           <p className="about-description">
             I specialize in building responsive and scalable applications using
-            React.js, Vue.js, Bootstrap, and CSS. Also have fundamental in UI/UX and familiar with figma. 
-            I have a keen eye for detail,
-            which helps me create reusable components that enhance code
-            maintainability and efficiency. Additionally, I’m familiar with REST
-            APIs, basic SQL, and MongoDB, and I have hands-on experience in
-            backend development to ensure smooth integrations across the stack.
+            React.js, Vue.js, Bootstrap, and CSS. Also have fundamental in UI/UX
+            and familiar with figma. I have a keen eye for detail, which helps
+            me create reusable components that enhance code maintainability and
+            efficiency. Additionally, I’m familiar with REST APIs, basic SQL,
+            and MongoDB, and I have hands-on experience in backend development
+            to ensure smooth integrations across the stack.
           </p>
           <p className="about-description">
             I believe in writing clean, readable code and embracing innovation
@@ -225,7 +239,7 @@ const About = () => {
       <Row className="mobile-only">
         <Col xs={12} sm={12} md={12} className="mx-3">
           <h2
-            ref={(el) => (subtitleRefs.current[1] = el)} 
+            ref={(el) => (subtitleRefs.current[1] = el)}
             className={`about-subtitle ${hoveredStates[1] ? "hovered" : ""}`}
           >
             Experiences
@@ -248,7 +262,7 @@ const About = () => {
       <Row className="mobile-only">
         <Col xs={12} sm={12} md={12} className="mx-3">
           <h2
-            ref={(el) => (subtitleRefs.current[2] = el)} 
+            ref={(el) => (subtitleRefs.current[2] = el)}
             className={`about-subtitle ${hoveredStates[2] ? "hovered" : ""}`}
           >
             Projects
